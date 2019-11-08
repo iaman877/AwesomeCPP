8b1699696c2899a16883e55a3563bdf4d2b0bd6a
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-11-08 22:01:32 +0530

    Initial

---
+++ Tree Data structure/Tree 1.c
@@ -0,0 +1,30 @@
+#include <iostream> 
+#include <vector> 
+using namespace std;
+template <typename T> 
+class TreeNode{ 
+    public : 
+      T data; 
+      vector <TreeNode<T>*> children; 
+      TreeNode(T data){ 
+          this->data = data; 
+      } 
+     void printTree(TreeNode<int> *root){ 
+         cout<<root->data<<endl; 
+         for(int i=0;i<root->children.size();i++) 
+         { 
+             printTree(root->children[i]); 
+         } 
+     } 
+}; 
+int main() { 
+   TreeNode<int> *root = new TreeNode<int>(1); 
+   TreeNode<int> *node1 = new TreeNode<int>(2); 
+   TreeNode<int> *node2 = new TreeNode<int>(3);  
+   root->children.push_back(node1); 
+   root->children.push_back(node2); 
+   root->printTree(root); 
+   return 0; 
+} 
+
+ 
