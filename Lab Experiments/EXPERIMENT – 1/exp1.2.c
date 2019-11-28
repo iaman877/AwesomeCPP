c1e51518041eb5628a95ce62bb45a33e70d96da0
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-11-28 20:13:39 +0530

    initial Commit

---
+++ Lab Experiments/EXPERIMENT – 1/exp1.2.c
@@ -0,0 +1,20 @@
+/* Convert Decimal number into a Binary Number using 1D array. */
+
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+  int a[10], n, i;    
+  cout<<"Enter the number to convert: ";    
+  cin>>n;    
+for(i=0; n>0; i++)    
+{    
+  a[i]=n%2;    
+  n= n/2;  
+}    
+  cout<<"Binary of the given number= ";    
+for(i=i-1 ;i>=0 ;i--)    
+ {    
+  cout<<a[i];    
+ }    
+}  
