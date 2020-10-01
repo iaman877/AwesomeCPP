e24b2a54c01f8afa48b0e05511961d7c5c04c015
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-10-01 22:06:09 +0530

    Added more functionalities  to the vector

--- Vectors/vector 1.c
+++ Vectors/vector 1.c
@@ -10,12 +10,23 @@ int main() {
       cout << "size:" << v.size() << endl; 
       v.push_back(i + 1); 
     } 
+    // user input in  a vector 
+    int n;
+    cin>>n;
+    for(int i=0; i<n;i++)
+    {
+         int a;
+         cin>>a;
+         v.push_back(a);
+    }
+      // manual  input 
       v.push_back(10); 
       v.push_back(20); 
       v.push_back(30); 
       v.push_back(23); 
       v.push_back(234); 
       v.pop_back(); 
+    // manual  output
             cout << v[0] << endl; 
             cout << v[1] << endl; 
             cout << v[2] << endl; 
@@ -25,5 +36,17 @@ int main() {
             cout << v[6] << endl; 
             cout << v.at(2) << endl; 
             cout << v.at(6) << endl; 
+    // auto loop  in vector
+    for(auto  it: v)
+    {
+     cout<<it<<" ";
+    }
+    // sorting in  a vector
+    sort(v.begin(),v.end());
+    // printing the  sorted vector
+    for(auto  it: v)
+    {
+     cout<<it<<" ";
+    }
         return 0;
  } 
